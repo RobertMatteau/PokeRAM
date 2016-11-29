72276e2288dcffb7036c5c434527d9f11cea7db9
Adam-Beckett-May <adam.may@uoit.net>
2016-11-28 23:46:22 -0500

    added commets

--- app/src/main/java/com/pokemonram/pokeram/PokemonMain.java
+++ app/src/main/java/com/pokemonram/pokeram/PokemonMain.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//The main menu of our application
 public class PokemonMain extends AppCompatActivity {
 
     MediaPlayer mp;
@@ -20,27 +21,28 @@ public class PokemonMain extends AppCompatActivity {
         Button moves = (Button)this.findViewById(R.id.button_moves_main);
         Button abilities = (Button)this.findViewById(R.id.button_abilities_main);
 
-
+        //This allows for the buttons to create sound when pushed
         mp = MediaPlayer.create(this,R.raw.rotom);
 
 
 
     }
 
-
+    //Starts the Pokedex Activity
     public void PokedexClick(View v){
         mp.start();
         Intent i = new Intent(this,PokedexActivity.class);
         startActivity(i);
-
     }
 
+    //Starts the Moves Activity
     public void MovesClick(View v){
         mp.start();
         Intent i = new Intent(this,MovesActivity.class);
         startActivity(i);
     }
 
+    //Starts the Abilities Activity
     public void AbilitiesClick(View v) {
         mp.start();
         Intent i = new Intent(this,AbilitiesActivity.class);
