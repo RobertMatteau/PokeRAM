46a72686379532c75066efbf992b2ca74f8698db
Adam-Beckett-May <adam.may@uoit.net>
2016-11-28 23:24:43 -0500

    added comments

--- app/src/main/java/com/pokemonram/pokeram/Data/Move.java
+++ app/src/main/java/com/pokemonram/pokeram/Data/Move.java
@@ -1,9 +1,11 @@
 package com.pokemonram.pokeram.Data;
 
 /**
- * Created by 100522340 on 11/25/2016.
+ * Created by 100522340 & 100487239 on 11/25/2016.
  */
 
+//The object Move
+//this object will hold the name of a technique and the typing of that move, both as a string
 public class Move {
     public String moveName;
     public String type;
